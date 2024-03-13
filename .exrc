341f2fc8c77aad2db58e7d9481e8787a364133d6
Serpent7776 <Serpent7776@gmail.com>
2024-03-13 22:57:08 +0100

    Startup+shutdown script test

---
+++ .exrc
@@ -0,0 +1,3 @@
+nmap <F9> :!./startup-test.sh<LF>
+nmap <F10> :!python whiskers.py 1.json<LF>
+nmap <F11> :!python whiskers2.py 1.json<LF>
---
+++ startup-test.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+touch empty
+
+hyperfine -N --warmup 1 --export-csv 1.csv --export-json 1.json \
+	"lua -e ''" \
+	"luajit -e ''" \
+	"sh -c ''" \
+	"bash -c ''" \
+	"zsh -c ''" \
+	"fish -c ''" \
+	"tclsh empty" \
+	"awk 'BEGIN{}'" \
+	"perl -e ''" \
+	"ruby -e ''" \
+	"python -c ''" \
+	"php -r ''" \
+	"node -e ''" \
+	"js60 -e ''" \
+	"js78 -e ''" \
+	"guile -c ''" \
+	"chicken-csi -p ''" \
+	"erl -eval 'halt()'" \
+	"julia -e ''" \
+	"ocaml empty" \
+	"swipl -q -c empty" \
+	"gprolog --init-goal halt" \
+	"scryer-prolog -g halt." \
+	"poly --script empty" \
+	"R -q -e '0'" \
+	"bqn -e '0'" \
+
